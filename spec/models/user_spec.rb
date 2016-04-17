9ba99ffd45c037082f47c44a48fd571f2c33021c
Meredith Bubier Ross <meredithbubier@icloud.com>
2016-04-17 14:18:41 -0400

    addressed hound violations, configured hound line length, added test for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_valid(:email).when("test@test.com") }
+  it { should_not have_valid(:email).when(nil, " ", "test@test") }
 end

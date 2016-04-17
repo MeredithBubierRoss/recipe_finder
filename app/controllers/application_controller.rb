eabcc6bdcde0c9078984d6ffd5477fa48ad2203b
Meredith Bubier Ross <meredithbubier@icloud.com>
2016-04-17 16:03:10 -0400

    address hound violations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password,
       :password_confirmation, :remember_me)
-     }
+    }
   end
 
   protect_from_forgery with: :exception

99191fc5fc794a7735b6b121aa66eb8774f51749
Meredith Bubier Ross <meredithbubier@icloud.com>
2016-05-06 16:16:47 -0400

    addressed hound violations

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -1,49 +1,49 @@
-$(function() {
-  $('form#new_recipe').submit(function(event)) {
-    event.preventDefault();
-    var newRecipeContent = $('#recipe_content').val();
-    var newRecipe = {
-      recipe: {
-        content: newRecipeContent
-      };
-    };
+$(function () {
+  $('form#new_recipe').submit(function (event) {
+        event.preventDefault();
+        var newRecipeContent = $('#recipe_content').val();
+        var newRecipe = {
+                recipe: {
+                    content: newRecipeContent
+                }
+            };
 
-    var request = $.ajax({
-      method: 'POST',
-      data: newRecipe,
-      url: '/recipes/new'
-    });
+        var request = $.ajax({
+                method: 'POST',
+                data: newRecipe,
+                url: '/recipes/new'
+            });
 
-    request.done(function() {
-      var html = '<div class ='callout success'>' + newRecipeContent + '</div>';
-      $('div.recipes').append(html);
-      $('#recipe_content').val('');
+        request.done(function () {
+            var html = '<div class ='callout success'>' + newRecipeContent + '</div>';
+            $('div.recipes').append(html);
+            $('#recipe_content').val('');
+        });
     });
-  };
 });
 
-$(function() {
-  $('form#edit_recipe').submit(function(event)) {
-    event.preventDefault();
-    var editRecipeContent = $('#recipe_content').val();
-    var editRecipe = {
-      recipe: {
-        content: editRecipeContent
-      };
-    };
+$(function () {
+  $('form#edit_recipe').submit(function (event) {
+        event.preventDefault();
+        var editRecipeContent = $('#recipe_content').val();
+        var editRecipe = {
+                recipe: {
+                    content: editRecipeContent
+                }
+            };
 
-  var request = $.ajax({
-    method: 'POST',
-    data: editRecipe,
-    url: '/recipes/:id/edit'
-  });
+        var request = $.ajax({
+                method: 'POST',
+                data: editRecipe,
+                url: '/recipes/:id/edit'
+            });
 
-  request.done(function() {
-    var html = '<div class ='callout success'>' + editRecipeContent + '</div>';
-      $('div.recipes').append(html);
-      $('#recipe_content').val('');
+        request.done(function () {
+            var html = '<div class ='callout success'>' + editRecipeContent + '</div>';
+            $('div.recipes').append(html);
+            $('#recipe_content').val('');
+        });
     });
-  };
 });
 
 var $modal = $('#modal');

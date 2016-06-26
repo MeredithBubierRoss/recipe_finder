a911ac2b5b2888e52fe7332b0db548d923b90cd0
Meredith Bubier Ross <meredithbubier@icloud.com>
2016-06-25 21:38:01 -0400

    addressed security issue with user showpages

--- spec/features/user_goes_to_profile_spec.rb
+++ spec/features/user_goes_to_profile_spec.rb
@@ -6,6 +6,8 @@ require 'rails_helper'
 #
 # [X] User logs into account
 # [X] User goes to profile page
+# [X] Unauthenticated user cannot access profiles
+# [] Authenticated user can only access personal profile
 
 feature "user goes to profile" do
   scenario "user signs in, goes to profile" do
@@ -21,5 +23,29 @@ feature "user goes to profile" do
 
     click_link @user.email
     expect(page).to have_content "Your profile"
+  end
+
+  scenario "unauthenticated user cannot access profiles" do
+    @user = FactoryGirl.create(:user)
+
+    visit("/users/#{@user.id}")
+
+    expect(page).to have_content "sign in or sign up before continuing"
+  end
+
+  scenario "authenticated user can only access personal profile" do
+    @user1 = FactoryGirl.create(:user)
+    @user2 = FactoryGirl.create(:user)
+
+    visit root_path
+    click_link "sign in"
+
+    fill_in "Email", with: @user1.email
+    fill_in "Password", with: @user1.password
+
+    click_button "Log in"
+
+    visit("users/#{@user2.id}")
+    expect(page).to have_content "you don't have access to this page"
   end
 end
